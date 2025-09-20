9bb5255a5ed7fb89ad925ab537d53dbff5a2876c
Serhiy Sokalskiy <sokalskiyserhiy1@gmail.com>
2025-09-20 15:21:23 +0300

    add feed endpoint

--- src/modules/feedParser/services/feed.db.ts
+++ src/modules/feedParser/services/feed.db.ts
@@ -1,4 +1,4 @@
-import type { PrismaClient } from "@prisma/client";
+import type { Feed, PrismaClient } from "@prisma/client";
 import type { FeedItem } from "../types/types";
 
 export async function saveFeedToDB(prisma: PrismaClient, feedData: FeedItem[]) {
@@ -14,7 +14,7 @@ export async function saveFeedToDB(prisma: PrismaClient, feedData: FeedItem[]) {
 }
 
 export async function getFeedFromDB(prisma: PrismaClient): Promise<FeedItem[]> {
-	const records = await prisma.feed.findMany();
+	const records: Feed[] = await prisma.feed.findMany();
 	return records.map((r) => ({
 		title: r.title,
 		contentSnippet: r.text,
